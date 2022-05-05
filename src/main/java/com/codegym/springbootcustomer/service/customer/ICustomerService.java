c670c451faeb1cfd5626cd6fb6a22a95fabf5c24
ducdaudshp <ducdaudshp@gmail.com>
2022-05-05 16:48:34 +0700

    quan ly customer

---
+++ src/main/java/com/codegym/springbootcustomer/service/customer/ICustomerService.java
@@ -0,0 +1,7 @@
+package com.codegym.springbootcustomer.service.customer;
+
+import com.codegym.springbootcustomer.model.Customer;
+import com.codegym.springbootcustomer.service.IGeneralService;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+}
